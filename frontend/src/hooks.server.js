a4d8004b5a634b662fbc080c381074da69f43380
Aimurat1 <aimurat927@gmail.com>
2024-04-09 23:47:51 +0500

    Update hooks.server.js for Production

--- frontend/src/hooks.server.js
+++ frontend/src/hooks.server.js
@@ -17,14 +17,14 @@ export const handle = async ({ event, resolve }) => {
     event.fetch = async (resource, init) => {
 
 		console.log(resource)
-        if (typeof resource === 'string' && (resource.startsWith('http://localhost:8000') || resource.startsWith('http://127.0.0.1:8000')) ) {
+        if (typeof resource === 'string' && (resource.startsWith('http://158.160.62.15:8000') || resource.startsWith('http://127.0.0.1:8000')) ) {
             // Modify the URL here if it matches your criteria
-            resource = resource.replace('http://localhost:8000', 'http://backend:8000');
+            resource = resource.replace('http://158.160.62.15:8000', 'http://backend:8000');
             // resource = resource.replace('http://127.0.0.1:8000', 'http://backend:8000');
 
-        } else if (resource instanceof Request && (resource.url.startsWith('http://localhost:8000') || resource.url.startsWith('http://127.0.0.1:8000'))) {
+        } else if (resource instanceof Request && (resource.url.startsWith('http://158.160.62.15:8000') || resource.url.startsWith('http://127.0.0.1:8000'))) {
             // For Request objects, modify the URL property
-            const newUrl = resource.url.replace('http://localhost:8000', 'http://backend:8000');
+            const newUrl = resource.url.replace('http://158.160.62.15:8000', 'http://backend:8000');
             // const newUrl = resource.url.replace('http://localhost:8000', 'http://backend:8000');
             resource = new Request(newUrl, resource);
         }
